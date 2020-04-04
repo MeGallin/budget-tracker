653cf25f6e64a1b8b403a203b14f5262ace62f29
Guy <me@garyallin.uk>
2020-04-04 11:45:38 +0100

    Added Edit feature

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
+const editButton = document.getElementById('edit-button');
+const submitButton = document.getElementById('submit-button');
+
 // const dummyTransactions = [
 //   { id: 1, text: 'Flower', amount: -20 },
 //   { id: 2, text: 'Salary', amount: 300 },
@@ -23,18 +26,20 @@ let transactions =
 // Add transaction
 let addTransaction = e => {
   e.preventDefault();
+
   if (text.value.trim() === '' || amount.value.trim() === '') {
-    alert('please values');
+    console.log('please values');
+    editButton.style.display = 'none';
   } else {
     const transaction = {
       id: randomId(),
       text: text.value,
       amount: parseInt(amount.value)
     };
+
     transactions.push(transaction);
     addTransactionDom(transaction);
     updateValues();
-
     updateLocalStorage();
 
     text.value = '';
@@ -53,19 +58,74 @@ let addTransactionDom = transaction => {
   const sign = transaction.amount < 0 ? '-' : '+';
 
   const item = document.createElement('li');
+  //   const inputItem = document.createElement('input');
+  //   item.setAttribute('id', 'editText');
 
   // Add Class based on income or expense
 
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
-  item.innerHTML = `
+  // Your button's onclick function's variables must be assigned with a quotes because it's value is String.
+  item.innerHTML = `   
+  <div class='wrapper'>
+    <div class='inner-wrapper' onClick = 'selectEditValue(
+        ${transaction.id},  
+        "${transaction.text}",
+        ${transaction.amount})'>    
         ${transaction.text} <span>${sign} ${Math.abs(
     transaction.amount
-  )}</span> <button class='delete-btn' onClick='removeTransaction(${
-    transaction.id
-  })'>X</button>
+  )}</span> 
+    </div>
+
+    <div>
+        <button class='delete-btn' onClick='removeTransaction(${
+          transaction.id
+        })'>
+            X
+        </button>
+    </div>
+  </div>
+  
     `;
   list.appendChild(item);
+};
+var inx;
+// Grab selected value to update
+let selectEditValue = (id, text, amount) => {
+  inx = id;
+  console.log(inx, text, amount);
+  document.getElementById('text').value = text;
+  document.getElementById('amount').value = amount;
+  submitButton.style.display = 'none';
+  editButton.style.display = 'block';
+};
+
+// Handle selected values to edit
+let handleSelectedValue = id => {
+  console.log(inx);
+  const textInputEdit = (document.getElementById('text').value = text.value);
+  const amountInputEdit = (document.getElementById('amount').value =
+    amount.value);
+
+  transactions.map((item, i) => {
+    if (item.id === inx) {
+      transactions[i] = {
+        id: inx,
+        text: textInputEdit,
+        amount: parseInt(amountInputEdit)
+      };
+    }
+  });
+
+  updateValues();
+  updateLocalStorage();
+
+  text.value = '';
+  amount.value = '';
+
+  submitButton.style.display = 'block';
+
+  init();
 };
 
 // Update the balance, income and expense
@@ -107,13 +167,14 @@ let updateValues = () => {
 
 // Delete funtionality
 let removeTransaction = id => {
-  transactions = transactions.filter(item => {
+  transactions = transactions.filter((item, i) => {
     if (item.id !== id) {
       return item;
     }
     updateLocalStorage();
     init();
   });
+  console.log(transactions);
 };
 
 // Update local storage transactions
@@ -123,10 +184,11 @@ let updateLocalStorage = () => {
 };
 
 // Init app
-let init = () => {
+let init = id => {
   list.innerHTML = '';
   transactions.forEach(addTransactionDom);
-  updateValues();
+  updateValues(id);
+  editButton.style.display = 'none';
 };
 
 init();
